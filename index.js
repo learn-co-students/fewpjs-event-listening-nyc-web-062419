eab7e7b1751995817bd74cd26d749955430e3fcd
nicholasstano <nicholasstano@gmail.com>
2019-08-06 12:42:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const input = document.getElementById("input");
+
 function addingEventListener() {
+  input.addEventListener("click", function(event) {
+    alert("I was clicked!");
+  });
 }
+
+// const input = document.querySelector('input');
+
+// function addingEventListener() {
+//   input.addEventListener('click', function(event) {
+//     return "clicked"
+//   });
+// }
